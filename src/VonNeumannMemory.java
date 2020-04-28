7ab25b2dfc307e40a314c0a6ab12ae0b296952fb
baahmed <basant.mounir@gmail.com>
2020-04-28 23:07:32 +0200

    Initial commit

---
+++ src/VonNeumannMemory.java
@@ -0,0 +1,21 @@
+
+public class VonNeumannMemory {
+	/*
+	 * this means one memory space for both the data and instructions.
+	 * THIS ALSO MEANS ONE ACCESS, EITHER DATA OR INSTRUCTIONS, AT A TIME!! (accessing data & instruction at the same TIME
+	 * => STRUCTURAL HAZARDa!
+	 * since we're allowed to structure it however we want... I'll do the first half as instructions and the second half as data.
+	 * it is BYTE ADDRESSABLE.
+	 */
+	
+	String[] memory;
+	int lastFreeDataByte;
+	int lastFreeInstructionByte;
+	
+	
+	
+	
+	
+
+	
+}
